d40ddf98200992d2f95c73b873a9c9ea2c470a69
REESE PETERSEN <reese.r.petersen@gmail.com>
2018-08-24 05:31:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,8 +25,11 @@ function pretendKitten(name) {
 }
 
 function removeLastKitten() {
-  return kittens.slice(0, length = -1);
+  return kittens.slice(0, kittens.length = -1);
 }
 
+function removeFirstKitten() {
+  return kittens.slice(1);
+}
 
 
